47077bcffd83c8e2a949001e64ba4eeaf09e32db
jmayta1984 <jmayta@pucp.edu.pe>
2024-01-31 23:35:06 -0500

    First commit

---
+++ Notificattions/app/src/main/java/pe/isil/myapplication/MainActivity.java
@@ -0,0 +1,99 @@
+package pe.isil.myapplication;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Button;
+import android.Manifest;
+
+import java.util.UUID;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btNotification;
+    private static final String CHANNEL_ID = "chaNnel001";
+
+
+    private void initViews() {
+        btNotification = findViewById(R.id.btNotification);
+    }
+
+    private void initViewListeners() {
+        btNotification.setOnClickListener(view -> {
+            createNotification();
+        });
+    }
+
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    createNotification();
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // feature requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
+            });
+
+
+    private void createNotification() {
+        checkNotificationPermission();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Notification")
+                .setContentText("Show a notification")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(UUID.randomUUID().hashCode(), builder.build());
+
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String name = "default";
+            String description = "description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        initViewListeners();
+        createNotificationChannel();
+    }
+
+    private void checkNotificationPermission() {
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.S_V2) {
+
+            String permission = Manifest.permission.POST_NOTIFICATIONS;
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissionLauncher.launch(permission);
+            }
+        }
+    }
+}
